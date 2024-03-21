4a0244e0f94b9e74090e49d0fd7c222f592c4faf
Adrian Netto <adrian.netto73@gmail.com>
2024-03-21 13:46:22 -0300

    test

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,16 +1,16 @@
-"use client"
-import React, { useEffect } from 'react'
+"use client";
 
-export default function Error({error, reset}) {
-
+import { useEffect } from "react";
+export default function Error({ error, reset }) {
   useEffect(() => {
     console.log(error);
-  }, [error])
-
+  }, [error]);
   return (
-    <div className='text-center mt-10'>
-      <h1>Something went wrong. Please, try again later.</h1>
-      <button className='hover:text-amber-600 bg-gray-800 p-2 mt-2 rounded' onClick={() => reset()}>Try Again</button>
+    <div className="text-center mt-10">
+      <h1>Something went wrong. Please try again later.</h1>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        Try Again
+      </button>
     </div>
-  )
+  );
 }
